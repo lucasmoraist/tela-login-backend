69a2a042009eaed3b13ffefdcc35a6dd23acd9a3
lucasmoraist <luksmnt1101@gmail.com>
2024-04-25 16:41:27 -0300

    feat: add requirements

--- src/main/java/com/lucas/loginauthapi/user/domain/User.java
+++ src/main/java/com/lucas/loginauthapi/user/domain/User.java
@@ -1,6 +1,7 @@
 package com.lucas.loginauthapi.user.domain;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
 import lombok.*;
 
 import java.util.UUID;
@@ -16,8 +17,14 @@ public class User {
 
     @Id @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+    @Min(3)
+    @Column(nullable = false, length = 120)
     private String name;
+    @Min(6)
+    @Column(nullable = false, unique = true)
     private String email;
+    @Min(6)
+    @Column(nullable = false)
     private String password;
 
 }
